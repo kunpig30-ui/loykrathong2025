b1ec339f076c61eae2558e08777ada0a67ac784c
kunpig30-ui <kunpig30@gmail.com>
2025-10-31 09:37:14 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME='loykrathong-v1';
+const URLS=[
+  './','index.html','manifest.json',
+  'images/bg5.png','images/bg5mb.png',
+  'images/kt1.png','images/kt2.png','images/kt3.png','images/kt4.png','images/kt5.png',
+  'images/tuktuk.png','images/logo.png',
+  'images/icon-192.png','images/icon-512.png',
+  'audio/song.mp3'
+];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS)))});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(fr=>{
+    return caches.open(CACHE_NAME).then(c=>{c.put(e.request,fr.clone());return fr;});
+  })).catch(()=>caches.match('images/bg5.png')));
+});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))))});
